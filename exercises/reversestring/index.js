d3babbe7d83da75d478aae0904f74a5833b5268e
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2021-12-25 22:52:15 +0600

    reverse a string using for loop

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,21 +7,21 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    debugger;
-    return str.split("").reduce((reversed,character) => character + reversed);
+    //Now make a reversed program in javascript.
+    const reversed = '';
+    for (let char = 0; char < str.length; char++){
+        reversed = str[char] + reversed;
+    }
+    return reversed;
 }
-reverse("jewel");
+
 module.exports = reverse;
 
+let a = [1,2,3,4,5]; 1-2 + 2-3 + 4-
 
 
 
-    // //Now make a reversed program using for loop.
-    // const reversed = '';
-    // for (let char = 0; char < str.length; char++){
-    //     reversed = str[char] + reversed;
-    // }
-    // return reversed;
+
 // const reversed = '';
 //     for(const character of str){
 //         reversed = character + reversed;
